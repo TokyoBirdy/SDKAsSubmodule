361567491a7dab380034c93dffba7b3b4cbab978
Cecilia Humlelu <cecilia.humlelu@screeninteraction.com>
2015-03-13 10:01:18 +0100

    this is going to be a submodule

---
+++ SDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDK
+//
+//  Created by Cecilia Humlelu on 13/03/15.
+//  Copyright (c) 2015 Cecilia Humlelu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SDKTests/SDKTests.swift
@@ -0,0 +1,36 @@
+//
+//  SDKTests.swift
+//  SDKTests
+//
+//  Created by Cecilia Humlelu on 13/03/15.
+//  Copyright (c) 2015 Cecilia Humlelu. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SDKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
